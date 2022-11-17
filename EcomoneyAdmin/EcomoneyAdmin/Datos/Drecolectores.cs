94000d6b0e4a2383a3d649a2e01c63bd1a38de9f
ale vel <ale.vrs01@outlook.com>
2022-11-17 13:52:49 -0500

    24 Conexion a Firebase

---
+++ EcomoneyAdmin/EcomoneyAdmin/Datos/Drecolectores.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EcomoneyAdmin.Modelo;
+using EcomoneyAdmin.Conexiones;
+using Firebase.Database.Query;
+
+namespace EcomoneyAdmin.Datos
+{
+    public class Drecolectores
+    {
+        public async Task<bool> InsertarRecolectores(Mrecolectores parametros)
+        {
+            await Constantes.firebase
+                .Child("Recolectores")
+                .PostAsync(new Mrecolectores()
+                {
+                    Correo = parametros.Correo,
+                    Estado = parametros.Estado,
+                    Identificacion = parametros.Identificacion,
+                    Nombre = parametros.Nombre
+                });
+            return true;
+        }
+    }
+}
---
+++ EcomoneyAdmin/EcomoneyAdmin/Modelo/Mrecolectores.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcomoneyAdmin.Modelo
+{
+    public class Mrecolectores
+    {
+        public string Estado { get; set; }
+        public string Correo { get; set; }
+        public string Nombre { get; set; }
+        public string Identificacion { get; set; }
+        public string Idrecolectores { get; set; }
+    }
+}
