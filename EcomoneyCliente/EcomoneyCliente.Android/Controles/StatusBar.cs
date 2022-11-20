188d08d435284cfc4237aafacd5e881e22380aeb
ale vel <ale.vrs01@outlook.com>
2022-11-19 21:04:42 -0500

    43 Agregar clases de Status bar

---
+++ EcomoneyCliente/EcomoneyCliente.Android/Controles/StatusBar.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EcomoneyCliente.Droid.Controles;
+using EcomoneyCliente.VistaModelo;
+using Xamarin.Forms;
+using Plugin.CurrentActivity;
+
+namespace EcomoneyCliente.Droid.Controles
+{
+    internal class StatusBar : VMstatusbar
+    {
+        public void MostrarStatusBar()
+        {
+            
+        }
+
+        public void OcultarStatusBar()
+        {
+            
+        }
+
+        public void TransparentarStatusbar()
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                var currentWindow = GetCurrentWindow();
+                currentWindow.DecorView.SystemUiVisibility = (StatusBarVisibility)SystemUiFlags.LayoutFullscreen;
+                currentWindow.SetStatusBarColor(Android.Graphics.Color.Transparent);
+            });
+        }
+
+        Window GetCurrentWindow()
+        {
+            var window = CrossCurrentActivity.Current.Activity.Window;
+            window.ClearFlags(WindowManagerFlags.TranslucentStatus);
+            window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+            return window;
+        }
+    }
+}
---
+++ EcomoneyCliente/EcomoneyCliente/VistaModelo/VMstatusbar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcomoneyCliente.VistaModelo
+{
+    public interface VMstatusbar
+    {
+        void OcultarStatusBar();
+        void MostrarStatusBar();
+        void TransparentarStatusbar();
+    }
+}
