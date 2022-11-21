053963f86e0a308f649d71023328391c46ef995d
ale vel <ale.vrs01@outlook.com>
2022-11-21 16:16:14 -0500

    53 Bloque detalle compra

---
+++ EcomoneyCliente/EcomoneyCliente/Datos/Ddetallecompra.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EcomoneyCliente.Conexiones;
+using EcomoneyCliente.Modelo;
+using Firebase.Database.Query;
+using System.Linq;
+
+namespace EcomoneyCliente.Datos
+{
+    public class Ddetallecompra
+    {
+        public async Task <List<Mdetallecompra>> MostrarDcompra(string Idcliente)
+        {
+            var data = (await Constantes.firebase
+                .Child("Detallecompra")
+                .OrderByKey()
+                .OnceAsync<Mdetallecompra>()).Where(a => a.Object.Idcliente == Idcliente);
+            
+            foreach(var item in data)
+            {
+                var parametros = new Mdetallecompra();
+                parametros.Preciocompra = "Precio de compra por " + item.Object.Und + " = S/." + item.Object.Preciocompra;
+                parametros.Cantidad = item.Object.Cantidad;
+                parametros.Total = "S/. " + item.Object.Total;
+                parametros.Und = item.Object.Und;
+            }
+        }
+    }
+}
---
+++ EcomoneyCliente/EcomoneyCliente/Modelo/Mdetallecompra.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcomoneyCliente.Modelo
+{
+    public class Mdetallecompra
+    {
+        public string Fecha { get; set; }
+        public string Ganancia { get; set; }
+        public string Idcliente { get; set; }
+        public string Idproducto { get; set; }
+        public string Cantidad { get; set; }
+        public string Preciocompra { get; set; }
+        public string PrecioVenta { get; set; }
+        public string Total { get; set; }
+        public string Und { get; set; }
+        public string Puntos { get; set; }
+    }
+}
