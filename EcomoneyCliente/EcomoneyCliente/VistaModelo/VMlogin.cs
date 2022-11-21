4bb4c52a0f27520e3b75acc6d4fa3fc366045d5c
ale vel <ale.vrs01@outlook.com>
2022-11-21 12:34:07 -0500

    46 Vista modelo Cliente

--- EcomoneyCliente/EcomoneyCliente/VistaModelo/VMlogin.cs
+++ EcomoneyCliente/EcomoneyCliente/VistaModelo/VMlogin.cs
@@ -1,15 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
+using EcomoneyCliente.Datos;
+using EcomoneyCliente.Modelo;
 
 namespace EcomoneyCliente.VistaModelo
 {
     public class VMlogin:BaseViewModel
     {
+        #region VARIABLES
+        public string identificacion;
+        #endregion
+
+        #region CONSTRUCTOR
         public VMlogin()
         {
             DependencyService.Get<VMstatusbar>().TransparentarStatusbar();
         }
+        #endregion
+
+        #region OBJETOS
+        public string txtidentificacion
+        {
+            get { return identificacion; }
+            set { SetValue(ref identificacion, value); }
+        }
+        #endregion
+
+        #region PROCESOS
+        private async Task validarLogin()
+        {
+            var funcion = new Dclientes();
+            var parametros = new Mclientes();
+            parametros.Identificacion = txtidentificacion;
+            var dt = await funcion.Validarcliente(parametros);
+
+            if(dt.Count > 0)
+            {
+                await Navigation.PushAsync(new Vistas.Menu());
+            }
+        }
+        #endregion
+
+        #region COMANDOS
+        public Command Logincommand { get; }
+        #endregion
     }
 }
