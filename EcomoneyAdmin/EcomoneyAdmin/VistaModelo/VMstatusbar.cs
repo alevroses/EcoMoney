ab1e0417420ae903da0fa05162ef643537b5e895
ale vel <ale.vrs01@outlook.com>
2022-11-24 15:25:41 -0500

    74 VM patrón + BaseViewModel

---
+++ EcomoneyAdmin/EcomoneyAdmin/VistaModelo/VMstatusbar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcomoneyAdmin.VistaModelo
+{
+    public interface VMstatusbar
+    {
+        void OcultarStatusBar();
+        void MostrarStatusBar();
+        void TransparentarStatusbar();
+    }
+}
---
+++ EcomoneyRecolector/EcomoneyRecolector/Datos/Drecolectores.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcomoneyRecolector.Datos
+{
+    public class Drecolectores
+    {
+    }
+}
